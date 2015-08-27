b198e93b358505798e71b50dcce2209d90e363b3
Harold Sanchez Balaguera <haroldsanchezb@gmail.com>
2015-08-27 17:54:13 -0300

    Check Files
    Steps:
    
    gulp shell
    gulp copy
    gulp test
    gulp delete

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,41 @@
 var gulp = require('gulp'),
-		del = require('del');
+		del = require('del'),
+		fs = require("fs"),
 		rename = require('gulp-rename'),
-		shell = require('gulp-shell'),
-		access = require('gulp-accessibility'),
+		run = require('gulp-run'),
+		access = require('gulp-accessibility');
 
 gulp.task('test', function() {
   return gulp.src('./templates/**/*.html')
     .pipe(access({accessibilityLevel: 'WCAG2AA', reportType: 'json', reportLocation : 'reports'}));
 });
 
-gulp.task('copy', function() {
-  return gulp.src('./originals/**/*.{tt,tpl}')
+gulp.task('shell', function () {
+	return run('git status').exec()
+		.pipe(gulp.dest('./templates')) // aqui se pondria svn st
+		.pipe(rename('file.txt'))
+    .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('copy', function () {
+	var srcFiles = [], 
+			fileContent = fs.readFileSync("./file.txt", "utf8"),
+			fileArray = fileContent.split("\n");
+	fileArray.forEach(function (element) {
+		if (element.search("modified:") !== -1 && // aqui se cambia a M 
+			(element.search(".tt")  !== -1 || element.search(".tpl")  !== -1) ) {
+			srcFiles.push((element.replace('modified:', '')).trim()); // aqui se cambia a M
+		}
+	});
+
+	return gulp.src(srcFiles)
   	.pipe(rename({extname: '.html'}))
   	.pipe(gulp.dest('./templates'));
 });
 
 gulp.task('delete', function(cb) {
-    del(['./templates'], cb)
+    del(['./templates/*'], cb)
 });
 
 gulp.task('watch', function() {
